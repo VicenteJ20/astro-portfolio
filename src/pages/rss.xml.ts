558a5b79a1410379b34ffd593285414ff4301494
Vicente Jorquera <jorquerav07@gmail.com>
2024-06-20 17:31:30 -0400

    feat/se agregan posts y projects a las rss
    feat/se agrega hoja de estilos, autor y tipo de post o proyecto al item

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,32 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 
+function capitalize(s: string) {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+
 export async function GET(context: any) {
   const posts = await getCollection('posts')
+  const projects = await getCollection('projects')
+
+  const items = [...posts, ...projects].map((item: any) => ({
+    title: `${item.data.title} | ${capitalize(item.data.type)}`,
+    description: item.data.description,
+    pubDate: item.data.pubDate,
+    link: `/${item.data.type === 'proyectos' ? item.data.type : 'blog'}/${item.data.slug}`,
+    customData: `
+      <language>es</language>
+      <author>${item.data.author.name}</author>
+    `
+  }))
+
   return rss({
+    stylesheet: '/rss/styles.xsl',
     title: 'Vicente Jorquera',
     description: 'Blog & portfolio de Vicente Jorquera',
     site: context.site,
-    items: posts.map((post: any) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      link: `/posts/${post.slug}`,
-      date: post.data.date,
-    })),
-    customData: `<language>es</language>`
+    items: items
   })
 }
