e1802efcfd651421bedacc0c237db20867a4390d
Vicente Jorquera <jorquerav07@gmail.com>
2024-06-21 17:41:12 -0400

    fix/se remueve console.log

--- src/utils/getCollectionByDate.ts
+++ src/utils/getCollectionByDate.ts
@@ -3,7 +3,6 @@ import { getCollection } from "astro:content";
 export async function getCollectionByDate(collectionName: any, limit?: number) {
   const collection = await getCollection(collectionName);
 
-  console.log('Funciona desde las utils')
   if (limit) {
     return collection.map((item: any) => ({ ...item, pubDate: new Date(item.data.pubDate) }))
       .sort((a, b) => b.pubDate - a.pubDate)
