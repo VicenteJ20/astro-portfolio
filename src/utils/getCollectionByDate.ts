678feafe2d9c240177282e57363ec9036025052a
Vicente Jorquera <jorquerav07@gmail.com>
2024-06-21 17:25:54 -0400

    feat/se agrega función para ordenar resultados de colecciones por fecha indicada en pubDate y permitiendo limitar los resultados de salida

---
+++ src/utils/getCollectionByDate.ts
@@ -0,0 +1,15 @@
+import { getCollection } from "astro:content";
+
+export async function getCollectionByDate(collectionName: any, limit?: number) {
+  const collection = await getCollection(collectionName);
+
+  console.log('Funciona desde las utils')
+  if (limit) {
+    return collection.map((item: any) => ({ ...item, pubDate: new Date(item.data.pubDate) }))
+      .sort((a, b) => b.pubDate - a.pubDate)
+      .slice(0, limit)
+  }
+
+  return collection.map((item: any) => ({ ...item, pubDate: new Date(item.data.pubDate) }))
+    .sort((a, b) => b.pubDate - a.pubDate)
+}
