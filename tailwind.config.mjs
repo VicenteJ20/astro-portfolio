745dbe529ea119b47ad2d78ec0216b9b5c2aea03
Vicente Jorquera <jorquerav07@gmail.com>
2024-06-21 21:19:17 -0400

    modify/se modifica ruta de styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'/styles/*.{css,scss,sass}',
+		'./public/styles/*.{css,scss,sass}',
+		'./public/styles/**/*.{css,scss,sass}', // Add this line
+	],
 	theme: {
 		extend: {
 			backdropBlur: {
